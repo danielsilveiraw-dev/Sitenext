2f83fdbf95c059defca7f1d003f56b94120398e3
Daniel silveira <danielsilveiraw@gmail.co>
2026-05-08 10:41:48 -0300

    Corrigido cookie do login Discord

--- app/api/auth/discord/route.ts
+++ app/api/auth/discord/route.ts
@@ -2,11 +2,19 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const clientId = process.env.DISCORD_CLIENT_ID;
+
   const redirectUri = encodeURIComponent(
     process.env.DISCORD_REDIRECT_URI!
   );
 
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=identify%20guilds`;
+  const scope = encodeURIComponent("identify guilds email");
+
+  const url =
+    `https://discord.com/oauth2/authorize` +
+    `?client_id=${clientId}` +
+    `&response_type=code` +
+    `&redirect_uri=${redirectUri}` +
+    `&scope=${scope}`;
 
   return NextResponse.redirect(url);
 }
