cda0b5eaf8e93b300d6a985dfb8d4f03082e0a6e
Daniel silveira <danielsilveiraw@gmail.co>
2026-05-09 14:31:25 -0300

    fix standalone config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
 
-  serverExternalPackages: ["@prisma/client", "prisma"],
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  serverExternalPackages: [
+    "@prisma/client",
+    "prisma"
+  ],
 };
 
 export default nextConfig;
