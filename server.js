ee50ce7fbb296a4c6a7c53f5551dc27b8a3b0a05
Daniel silveira <danielsilveiraw@gmail.co>
2026-05-15 20:14:32 -0300

    remove build do server

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const { execSync } = require('child_process')
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-console.log('> Building...')
-execSync('npm run build', { stdio: 'inherit' })
-console.log('> Build completo!')
+const port = process.env.PORT || 8080;
 
-const app = next({ dev: false })
-const handle = app.getRequestHandler()
+const app = next({
+  dev: false,
+  hostname: "0.0.0.0",
+  port,
+});
+
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(8080, '0.0.0.0', () => {
-    console.log('> Ready on http://0.0.0.0:8080')
-  })
-})
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, "0.0.0.0", () => {
+    console.log(`> Ready on http://0.0.0.0:${port}`);
+  });
+});
