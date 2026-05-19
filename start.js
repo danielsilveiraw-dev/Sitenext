a0a14a248fd0cf7bdde4781115afbee0e95a0303
Daniel silveira <danielsilveiraw@gmail.co>
2026-05-19 02:16:52 -0300

    agora

---
+++ start.js
@@ -0,0 +1,27 @@
+const { execSync } = require("child_process");
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+console.log("> Running next build...");
+execSync("npx next build", { stdio: "inherit" });
+console.log("> Build complete, starting server...");
+
+const port = process.env.PORT || 8080;
+
+const app = next({
+  dev: false,
+  hostname: "0.0.0.0",
+  port,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, "0.0.0.0", () => {
+    console.log(`> Ready on http://0.0.0.0:${port}`);
+  });
+});
